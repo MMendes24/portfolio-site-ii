bd9cb25ad7caa4e64644bdd92d884cd61d2bbb38
MMendes24 <mmbusiness1@protonmail.com>
2020-09-22 21:42:58 -0400

    Finally picked a background image.

--- src/styles/AppStyled.js
+++ src/styles/AppStyled.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
 
 const AppStyled = styled.div`
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
     width: 80%;
+    margin: auto;
     background-color: white;
+    opacity: 0.8;
 `
 export default AppStyled
