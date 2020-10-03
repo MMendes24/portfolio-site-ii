97f731dda7db78a7b5d9f708b6cd86ac99267e5b
MMendes24 <mmbusiness1@protonmail.com>
2020-10-03 11:43:01 -0400

    Bugfixing on main for simplicity's sake.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import NavBar from './NavBar'
 import Landing from './Landing'
 import FooterLinks from './FooterLinks'
-import AboutMe from './AboutMe.js'
+import AboutMe from './AboutMe'
 
 export {
     NavBar,
