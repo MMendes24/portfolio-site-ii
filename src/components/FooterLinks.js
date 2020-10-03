16a79062bc2778406f37c4b2616bb059c8c8f56e
MMendes24 <mmbusiness1@protonmail.com>
2020-10-03 14:31:18 -0400

    Reformatting code to be more legible.

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -5,15 +5,19 @@ import { FooterStyled as Footer } from '../styles'
 const FooterLinks = () => {
     return (
         <Footer>
+
             <a className="social-link" href="https://github.com/MMendes24">
                 <i class="fab fa-github-square"></i>
             </a>
+
             <a className="social-link"  href="https://twitter.com/MarsMendes2">
                 <i class="fab fa-twitter-square" ></i>
             </a>
+
             <a className="social-link"  href="https://www.linkedin.com/in/marsdmendes/">
                 <i class="fab fa-linkedin"></i>
             </a>
+            
         </Footer>
     )
 }
